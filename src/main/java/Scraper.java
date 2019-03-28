1197b737af5fe854c26ebb5f25b90aac82de4253
bragalotwill <williambraga777@gmail.com>
2019-03-28 15:45:39 -0400

    Removed While Loop

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -14,75 +14,71 @@ import javax.mail.internet.MimeMessage;
 public class Scraper {
 
     final static String apartmentURL = "https://tracemidtown.com/floorplans/";
-    final static long timeInterval = 86400000;
     final static String username = "webscraperapartments@gmail.com";
     final static String password = "webscraper123";
     final static String recipient = "bragalotwill@gmail.com";
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        while(true) {
-            Document floorplans = Jsoup.connect(apartmentURL).get();
-            List<Apartment> apartments = new ArrayList<>();
-            List<Apartment> augustApartments = new ArrayList<>();
-            for (Element e : floorplans.select("a")) {
-                String link = e.attr("href");
-                if (link.contains(apartmentURL)) {
-                    Document floorplan = Jsoup.connect(link).get();
-                    System.out.println(floorplan.title());
-                    String fpType = floorplan.title().split(" \\|")[0];
-                    Apartment found = null;
-                    for (Element e2 : floorplan.select("td")) {
-                        if (e2.select("span").toString().contains("Rent")) {
-                            String rent = e2.ownText();
-                            found.setRent(rent);
+        Document floorplans = Jsoup.connect(apartmentURL).get();
+        List<Apartment> apartments = new ArrayList<>();
+        List<Apartment> augustApartments = new ArrayList<>();
+        for (Element e : floorplans.select("a")) {
+            String link = e.attr("href");
+            if (link.contains(apartmentURL)) {
+                Document floorplan = Jsoup.connect(link).get();
+                System.out.println(floorplan.title());
+                String fpType = floorplan.title().split(" \\|")[0];
+                Apartment found = null;
+                for (Element e2 : floorplan.select("td")) {
+                    if (e2.select("span").toString().contains("Rent")) {
+                        String rent = e2.ownText();
+                        found.setRent(rent);
+                    }
+                    if (e2.select("span").toString().contains("Availability")) {
+                        String availability = e2.ownText();
+                        if (!availability.contains("Available Now")) {
+                            availability = availability.split("on ")[1];
                         }
-                        if (e2.select("span").toString().contains("Availability")) {
-                            String availability = e2.ownText();
-                            if (!availability.contains("Available Now")) {
-                                availability = availability.split("on ")[1];
-                            }
-                            found.setMoveIn(availability);
-                            apartments.add(found);
-                            if (found.getMoveIn().contains("August")) {
-                                augustApartments.add(found);
-                            }
-                            found = null;
+                        found.setMoveIn(availability);
+                        apartments.add(found);
+                        if (found.getMoveIn().contains("August")) {
+                            augustApartments.add(found);
                         }
-                        if (e2.select("span").toString().contains("Apt #")) {
-                            found = new Apartment(fpType);
-                            String apNum = e2.ownText();
-                            found.setApNum(Integer.parseInt(apNum));
-                        }
+                        found = null;
+                    }
+                    if (e2.select("span").toString().contains("Apt #")) {
+                        found = new Apartment(fpType);
+                        String apNum = e2.ownText();
+                        found.setApNum(Integer.parseInt(apNum));
                     }
                 }
             }
-            if (augustApartments.size() > 0) {
-                Properties prop = new Properties();
-                prop.put("mail.smtp.auth", "true");
-                prop.put("mail.smtp.starttls.enable", "true");
-                prop.put("mail.smtp.host", "smtp.gmail.com");
-                prop.put("mail.smtp.port", "587");
-                Session session = Session.getInstance(prop, new Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
-                try {
-                    Message message = new MimeMessage(session);
-                    message.setFrom(new InternetAddress(username));
-                    message.setRecipients(
-                            Message.RecipientType.TO,
-                            InternetAddress.parse(recipient));
-                    message.setSubject("August Apartment Available!");
-                    message.setText(makeMessage(augustApartments));
-                    Transport.send(message);
-                    System.out.println("Sent!");
-                } catch(MessagingException e) {
-                    throw new RuntimeException(e);
+        }
+        if (augustApartments.size() > 0) {
+            Properties prop = new Properties();
+            prop.put("mail.smtp.auth", "true");
+            prop.put("mail.smtp.starttls.enable", "true");
+            prop.put("mail.smtp.host", "smtp.gmail.com");
+            prop.put("mail.smtp.port", "587");
+            Session session = Session.getInstance(prop, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
                 }
+            });
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(username));
+                message.setRecipients(
+                        Message.RecipientType.TO,
+                        InternetAddress.parse(recipient));
+                message.setSubject("August Apartment Available!");
+                message.setText(makeMessage(augustApartments));
+                Transport.send(message);
+                System.out.println("Sent!");
+            } catch(MessagingException e) {
+                throw new RuntimeException(e);
             }
-            Thread.sleep(timeInterval);
         }
     }
     public static String makeMessage(List<Apartment> apartments) {
